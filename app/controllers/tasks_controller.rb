0277d957a11aa8e77b44ea518093abd16e26855a
attack20 <atack300@gmail.com>
2017-10-03 12:14:37 +0000

    commitです。

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -59,7 +59,7 @@ class TasksController < ApplicationController
     @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
-    redirect_to messages_url
+    redirect_to tasks_url
     end
   
   private
