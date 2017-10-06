1e8ca46cca4b236f0a63d810f6500d445ccec3a9
attack20 <atack300@gmail.com>
2017-10-06 06:07:01 +0000

    一応できました。

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,16 +1,16 @@
 class TasksController < ApplicationController
+   before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   
   def index
-     @tasks = Task.all
+     @tasks = Task.all.page(params[:page])
   end
   def show
-    @task = Task.find(params[:id])
   end
     def new
       @task = Task.new
     end
     def edit
-     @task = Task.find(params[:id])
     end
     
     def create
@@ -26,7 +26,6 @@ class TasksController < ApplicationController
     end
     
     def update
-    @task = Task.find(params[:id])
 
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
@@ -38,7 +37,6 @@ class TasksController < ApplicationController
     end
     
     def destroy
-    @task = Task.find(params[:id])
     @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
@@ -48,6 +46,9 @@ class TasksController < ApplicationController
   private
 
   # Strong Parameter
+   def set_task
+    @task = Task.find(params[:id])
+   end
   
   def task_params
     params.require(:task).permit(:content)
